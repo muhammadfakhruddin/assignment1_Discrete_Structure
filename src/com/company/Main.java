177f9b923fcb70a4902d01a268d60ab3b559d657
Fakhruddin <alanberamboi@gmail.com>
2021-05-02 00:31:07 +0800

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,23 +5,26 @@ import java.util.Scanner;
 public class Main {
     public static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-    System.out.println("TOPIC 5: Division in the Integer: Prime Number and Prime Factorization");
-    System.out.println("----------------------------------------------------------------------");
-    System.out.println("Press 1 : Prime Number");
-    System.out.println("Press 2 : Prime Factorization");
-    System.out.print("Choose 1 or 2  : ");
-    int choose = scan.nextInt();
+        int choose = 0;
+        do {
+            System.out.println("TOPIC 5: Division in the Integer: Prime Number and Prime Factorization");
+            System.out.println("----------------------------------------------------------------------");
+            System.out.println("Press 1 : Prime Number");
+            System.out.println("Press 2 : Prime Factorization");
+            System.out.println("Press 0 to Exit");
+            System.out.print("Choose : ");
+            choose = scan.nextInt();
 
-    if (choose == 1) {
-        System.out.println("Prime Number");
-        System.out.println("------------");
-        primeNumber();
-    } else if (choose == 2) {
-        System.out.println("Prime Factorization");
-        System.out.println("-------------------");
-        primeFactor();
-    }
-
+            if (choose == 1) {
+                System.out.println("Prime Number");
+                System.out.println("------------");
+                primeNumber();
+            } else if (choose == 2) {
+                System.out.println("Prime Factorization");
+                System.out.println("-------------------");
+                primeFactor();
+            }
+        }while (choose!=0);
     }
 
     private static void primeNumber() {
@@ -41,6 +44,9 @@ public class Main {
             }
             if(flag==0)  { System.out.println(n+" is prime number"); }
         }//end of else
+        System.out.println("Press enter to continue");
+        try{System.in.read();}
+        catch(Exception e){}
     }
 
     private static void primeFactor() {
@@ -56,5 +62,8 @@ public class Main {
         if(number >2) {
             System.out.println(number);
         }
+        System.out.println("Press enter to continue");
+        try{System.in.read();}
+        catch(Exception e){}
     }
 }
